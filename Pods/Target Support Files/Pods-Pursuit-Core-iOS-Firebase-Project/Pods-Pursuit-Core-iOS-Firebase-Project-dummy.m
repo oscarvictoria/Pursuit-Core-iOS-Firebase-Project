c49d63d53ee7b907a9f3678bd7ed728cbcb149d9
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-03-06 13:11:16 -0500

    updated project

---
+++ Pods/Target Support Files/Pods-Pursuit-Core-iOS-Firebase-Project/Pods-Pursuit-Core-iOS-Firebase-Project-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pursuit_Core_iOS_Firebase_Project : NSObject
+@end
+@implementation PodsDummy_Pods_Pursuit_Core_iOS_Firebase_Project
+@end
---
+++ Pods/Target Support Files/Pods-Pursuit-Core-iOS-Firebase-Project/Pods-Pursuit-Core-iOS-Firebase-Project-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Pursuit_Core_iOS_Firebase_ProjectVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Pursuit_Core_iOS_Firebase_ProjectVersionString[];
+
