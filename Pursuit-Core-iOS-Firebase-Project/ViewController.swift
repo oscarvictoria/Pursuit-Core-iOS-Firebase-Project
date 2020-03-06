b11290d35c71e6db88981242aba86c8d667e50e4
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-03-06 12:05:09 -0500

    Initial Commit

---
+++ Pursuit-Core-iOS-Firebase-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pursuit-Core-iOS-Firebase-Project
+//
+//  Created by Oscar Victoria Gonzalez  on 3/6/20.
+//  Copyright © 2020 Oscar Victoria Gonzalez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
